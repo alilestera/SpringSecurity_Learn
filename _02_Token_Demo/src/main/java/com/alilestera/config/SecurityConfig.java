073d4548cba8160950990e78acb42d0bb2a3f5ba
Alilestera <alilestera1404@outlook.com>
2022-02-22 15:54:47 +0800

    add JwtAuthenticationTokenFilter clazz

--- _02_Token_Demo/src/main/java/com/alilestera/config/SecurityConfig.java
+++ _02_Token_Demo/src/main/java/com/alilestera/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.alilestera.config;
 
+import com.alilestera.filter.JwtAuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author Alilestera
@@ -27,6 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -40,5 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user/login").anonymous()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
---
+++ _02_Token_Demo/src/main/java/com/alilestera/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,63 @@
+package com.alilestera.filter;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alilestera.domain.LoginUser;
+import com.alilestera.utils.JwtUtil;
+import com.alilestera.utils.RedisCache;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Alilestera
+ * @date 2/22/2022
+ */
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //获取token
+        String token = request.getHeader("token");
+        if (!StringUtils.hasText(token)) {
+            //如果获取不到token，则放行
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //解析token
+        String userId;
+        try {
+            Claims claims = JwtUtil.parseJWT(token);
+            userId = claims.getSubject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("token非法");
+        }
+        //从redis中获取用户信息
+        //取出的是Json对象，必须使用toJavaObject方法转换成LoginUser对象
+        //否则会报无法转换类型的异常
+        String redisKey = "login:" + userId;
+        JSONObject jsonObject = redisCache.getCacheObject(redisKey);
+        LoginUser loginUser = jsonObject.toJavaObject(LoginUser.class);
+        //存入SecurityContextHolder
+        //TODO 获取权限信息封装到Authentication中
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginUser, null, null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        //放行
+        filterChain.doFilter(request, response);
+    }
+}
